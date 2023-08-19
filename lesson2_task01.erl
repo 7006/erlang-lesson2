2b5c9d177b935996f9fad67e0ce17da5acc02925
7006 <22399553+7006@users.noreply.github.com>
2023-08-19 15:44:48 +0000

    Add comments describing function purpose

--- lesson2_task01.erl
+++ lesson2_task01.erl
@@ -1,6 +1,7 @@
 -module(lesson2_task01).
 -export([last/1]).
 
+% Знайти останній елемент списку
 last([]) ->
     false;
 last([H]) ->
--- lesson2_task02.erl
+++ lesson2_task02.erl
@@ -1,5 +1,6 @@
 -module(lesson2_task02).
 -export([but_last/1]).
 
+% Знайти два останніх елементи списку
 but_last([]) ->
     ok.

8dd50f7ba180094a262e9b61abd866d46e991ddc
7006 <22399553+7006@users.noreply.github.com>
2023-08-19 16:01:13 +0000

    Add failed tests for lesson2_task02 module

--- lesson2_task02_tests.erl
+++ lesson2_task02_tests.erl
@@ -6,7 +6,23 @@
 but_last_test_() ->
     [
         {
-            "it should return atom 'ok'",
-            ?_assertEqual(ok, but_last([]))
+            "it should return atom 'false' for an empty list",
+            ?_assertEqual(false, but_last([]))
+        },
+        {
+            "it should return atom 'false' for the one-element list",
+            ?_assertEqual(false, but_last([a]))
+        },
+        {
+            "it should return 2 last elements for the two-element list",
+            ?_assertEqual({a, b}, but_last([a, b]))
+        },
+        {
+            "it should return 2 last elements for the three-element list",
+            ?_assertEqual({b, c}, but_last([a, b, c]))
+        },
+        {
+            "it should return 2 last elements for a non empty list",
+            ?_assertEqual({e, f}, but_last([a, b, c, d, e, f]))
         }
     ].

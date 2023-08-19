9fe7d949cb13b01e50d200fe6601eeb79a1488d0
7006 <22399553+7006@users.noreply.github.com>
2023-08-19 20:35:12 +0000

    Add element_at/2

--- lesson2_task03.erl
+++ lesson2_task03.erl
@@ -2,5 +2,9 @@
 -export([element_at/2]).
 
 % Знайти N-й елемент списку
-element_at(List, Index) ->
-    ok.
+element_at([], _) ->
+    undefined;
+element_at([H | _], 1) ->
+    H;
+element_at([_ | T], Index) ->
+    element_at(T, Index - 1).

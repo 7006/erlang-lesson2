57f2ff6fd9f83cd02a6f9f54557a511d190f61f0
7006 <22399553+7006@users.noreply.github.com>
2023-08-19 21:39:12 +0000

    Add implementation of len/1 function

--- lesson2_task04.erl
+++ lesson2_task04.erl
@@ -2,5 +2,10 @@
 -export([len/1]).
 
 % Порахувати кількість елементів списку
-len([]) ->
-    ok.
+len(L) when is_list(L) ->
+    len_(L, 0).
+
+len_([], Acc) ->
+    Acc;
+len_([_ | T], Acc) ->
+    len_(T, 1 + Acc).
--- lesson2_task04_tests.erl
+++ lesson2_task04_tests.erl
@@ -6,7 +6,22 @@
 len_test_() ->
     [
         {
-            "it should return atom 'ok'",
-            ?_assertEqual(ok, len([]))
+            "it should reject non list value for the 'List' argument",
+            [
+                ?_assertError(function_clause, len(0)),
+                ?_assertError(function_clause, len({}))
+            ]
+        },
+        {
+            "it should return 0 for an empty list",
+            ?_assertEqual(0, len([]))
+        },
+        {
+            "it should return the length of a non empty list",
+            [
+                ?_assertEqual(4, len([a, b, c, d])),
+                ?_assertEqual(2, len([a, b])),
+                ?_assertEqual(1, len([a]))
+            ]
         }
     ].

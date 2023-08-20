2b2013af81bfc29e4f785cdf962c12ff9636aee2
7006 <22399553+7006@users.noreply.github.com>
2023-08-20 15:26:17 +0000

    Move example of step by step execution of function replicate/2 to standalone file

--- lesson2_task15.erl
+++ lesson2_task15.erl
@@ -16,25 +16,3 @@ replicate([_ | T], Count, Acc, 0) ->
     replicate(T, Count, Acc, Count);
 replicate([H | T], Count, Acc, Current) ->
     replicate([H | T], Count, [H | Acc], Current - 1).
-
-% abc 3
-% abc 3 [] 3
-
-% a bc 3 [] 3
-% a bc 3 a  2
-% a bc 3 aa 1
-% a bc 3 aaa 0
-
-% bc 3 aaa 3
-
-% b c 3 aaa 3
-% b c 3 baaa 2
-% b c 3 bbaaa 1
-% b c 3 bbbaaa 0
-
-% c 3 bbbaaa 3
-% c 3 cbbbaaa 2
-% c 3 ccbbbaaa 1
-% c 3 cccbbbaaa 0
-
-% [] 3 cccbbbaaa 3

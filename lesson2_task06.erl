7298f6aee350f5e8384776565523f4b7ed6fd9f4
7006 <22399553+7006@users.noreply.github.com>
2023-08-20 08:48:01 +0000

    Add is_palindrome/1

--- lesson2_task06.erl
+++ lesson2_task06.erl
@@ -1,6 +1,15 @@
 -module(lesson2_task06).
 -export([is_palindrome/1]).
 
+-import(lesson2_task05, [reverse/1]).
+
 % Визначити, чи є список паліндромом
-is_palindrome([]) ->
-    true.
+is_palindrome(List) when is_list(List) ->
+    is_palindrome(List, reverse(List)).
+
+is_palindrome([], []) ->
+    true;
+is_palindrome([H | T1], [H | T2]) ->
+    is_palindrome(T1, T2);
+is_palindrome(_, _) ->
+    false.

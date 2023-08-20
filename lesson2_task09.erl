4fc67a6ba004d6e106cb6e82f4f7912ee5cecebc
7006 <22399553+7006@users.noreply.github.com>
2023-08-20 18:55:22 +0000

    Add pack/1 implementation

--- lesson2_task09.erl
+++ lesson2_task09.erl
@@ -1,6 +1,15 @@
 -module(lesson2_task09).
 -export([pack/1]).
 
+-import(lesson2_task05, [reverse/1]).
+
 % Запакувати послідовні дублікати у вкладені списки
-pack([]) ->
-    ok.
+pack(List) when is_list(List) ->
+    pack(List, [], []).
+
+pack([], [], Acc) ->
+    reverse(Acc);
+pack([H, H | T], Dup, Acc) ->
+    pack([H | T], [H | Dup], Acc);
+pack([H | T], Dup, Acc) ->
+    pack(T, [], [[H | Dup] | Acc]).

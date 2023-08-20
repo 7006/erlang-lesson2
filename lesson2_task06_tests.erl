0a4a38ffaa692098b8dbd5288e94e91b51a4740c
7006 <22399553+7006@users.noreply.github.com>
2023-08-20 07:56:12 +0000

    Replace assertEqual macro to assert assertNot macros

--- lesson2_task06_tests.erl
+++ lesson2_task06_tests.erl
@@ -8,31 +8,31 @@ is_palindrome_test_() ->
         {
             "it should return 'true' for a palindrome list",
             [
-                ?_assertEqual(true, is_palindrome([1, 2, 3, 2, 1])),
-                ?_assertEqual(true, is_palindrome([10, 20, 20, 10])),
-                ?_assertEqual(true, is_palindrome([r, o, t, o, r])),
-                ?_assertEqual(true, is_palindrome([a, b, b, a]))
+                ?_assert(is_palindrome([1, 2, 3, 2, 1])),
+                ?_assert(is_palindrome([10, 20, 20, 10])),
+                ?_assert(is_palindrome([r, o, t, o, r])),
+                ?_assert(is_palindrome([a, b, b, a]))
             ]
         },
         {
             "it should consider an empty list as palindrome",
-            ?_assertEqual(true, is_palindrome([]))
+            ?_assert(is_palindrome([]))
         },
         {
             "it should consider a one-element list as palindrome",
             [
-                ?_assertEqual(true, is_palindrome([a])),
-                ?_assertEqual(true, is_palindrome([0])),
-                ?_assertEqual(true, is_palindrome([2]))
+                ?_assert(is_palindrome([a])),
+                ?_assert(is_palindrome([0])),
+                ?_assert(is_palindrome([2]))
             ]
         },
         {
             "it should return 'false' for a non palindrome list",
             [
-                ?_assertEqual(false, is_palindrome([1, 2, 3, 4])),
-                ?_assertEqual(false, is_palindrome([1, 2, 3, 4, 2, 1])),
-                ?_assertEqual(false, is_palindrome([m, o, t, o, r])),
-                ?_assertEqual(false, is_palindrome([a, b, o, k, n, o, b, a]))
+                ?_assertNot(is_palindrome([1, 2, 3, 4])),
+                ?_assertNot(is_palindrome([1, 2, 3, 4, 2, 1])),
+                ?_assertNot(is_palindrome([m, o, t, o, r])),
+                ?_assertNot(is_palindrome([a, b, o, k, n, o, b, a]))
             ]
         }
     ].

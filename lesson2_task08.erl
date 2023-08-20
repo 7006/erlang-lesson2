099c16c4447c1032f8a50c3f2e6d7b0af555b168
7006 <22399553+7006@users.noreply.github.com>
2023-08-20 16:12:05 +0000

    Add compress/1 implementation

--- lesson2_task08.erl
+++ lesson2_task08.erl
@@ -1,6 +1,15 @@
 -module(lesson2_task08).
 -export([compress/1]).
 
+-import(lesson2_task05, [reverse/1]).
+
 % Видалити послідовні дублікати
-compress([]) ->
-    ok.
+compress(List) when is_list(List) ->
+    compress(List, []).
+
+compress([], Acc) ->
+    reverse(Acc);
+compress([H, H | T], Acc) ->
+    compress([H | T], Acc);
+compress([H | T], Acc) ->
+    compress(T, [H | Acc]).
---
+++ lesson2_task08.md
@@ -0,0 +1,16 @@
+Example of step by step execution of function `compress/1`
+
+|List|Acc|
+|----|---|
+|aaaabbbcddd|[]|
+|aaabbbcddd |[]|
+|aabbbcddd |[]|
+|abbbcddd |[]|
+|bbbcddd |a|
+|bbcddd |a|
+|bcddd |a|
+|cddd |ba|
+|ddd |cba|
+|dd |cba|
+|d |cba|
+|[] |dcba|

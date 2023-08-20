86579899a4055e41ee74c7283c79fba5bb726f52
7006 <22399553+7006@users.noreply.github.com>
2023-08-20 15:42:42 +0000

    Add failed tests for compress/1

--- lesson2_task08.erl
+++ lesson2_task08.erl
@@ -1,5 +1,6 @@
 -module(lesson2_task08).
 -export([compress/1]).
 
+% Видалити послідовні дублікати
 compress([]) ->
     ok.
--- lesson2_task08_tests.erl
+++ lesson2_task08_tests.erl
@@ -6,7 +6,45 @@
 compress_test_() ->
     [
         {
-            "it should return atom 'ok'",
-            ?_assertEqual(ok, compress([]))
+            "it should remove consequential duplicate elements from a non-empty list",
+            [
+                ?_assertEqual(
+                    [a, b, c, a, d, e],
+                    compress([a, a, a, a, b, c, c, a, d, e, e, e, e])
+                ),
+                ?_assertEqual(
+                    [a, b, c, d],
+                    compress([a, b, c, c, d])
+                ),
+                ?_assertEqual(
+                    [a, b, c, d],
+                    compress([a, a, b, c, d])
+                ),
+                ?_assertEqual(
+                    [a, b, c, d],
+                    compress([a, b, c, d, d])
+                )
+            ]
+        },
+        {
+            "it should return the same list for a non-empty list with unique elements",
+            [
+                ?_assertEqual(
+                    [a, b, c, d],
+                    compress([a, b, c, d])
+                ),
+                ?_assertEqual(
+                    [a, b],
+                    compress([a, b])
+                ),
+                ?_assertEqual(
+                    [a],
+                    compress([a])
+                )
+            ]
+        },
+        {
+            "it should return an empty list when you pass an empty list",
+            ?_assertEqual([], compress([]))
         }
     ].

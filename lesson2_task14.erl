03e70dbc77312e5a43d984ab2e60b8f7872a9304
7006 <22399553+7006@users.noreply.github.com>
2023-08-20 09:06:09 +0000

    Add failed tests for duplicate/1

--- lesson2_task14.erl
+++ lesson2_task14.erl
@@ -1,5 +1,6 @@
 -module(lesson2_task14).
 -export([duplicate/1]).
 
+% Написати дублікатор усіх елементів списку
 duplicate([]) ->
     ok.
--- lesson2_task14_tests.erl
+++ lesson2_task14_tests.erl
@@ -6,7 +6,24 @@
 duplicate_test_() ->
     [
         {
-            "it should return atom 'ok'",
-            ?_assertEqual(ok, duplicate([]))
+            "it should duplicate consequential elements for a non-empty list",
+            [
+                ?_assertEqual(
+                    [a, a, b, b, c, c, c, c, d, d],
+                    duplicate([a, b, c, c, d])
+                ),
+                ?_assertEqual(
+                    [a, a, a, a, b, b, c, c, c, c, d, d, d, d],
+                    duplicate([a, a, b, c, c, d, d])
+                ),
+                ?_assertEqual(
+                    [a, a],
+                    duplicate([a])
+                )
+            ]
+        },
+        {
+            "it should return an empty list when you pass an empty list",
+            ?_assertEqual([], duplicate([]))
         }
     ].

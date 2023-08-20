84b2d43c47c5141262b295d9c295244b3b06ad82
7006 <22399553+7006@users.noreply.github.com>
2023-08-20 16:45:16 +0000

    Add failed tests for pack/1

--- lesson2_task09_tests.erl
+++ lesson2_task09_tests.erl
@@ -6,7 +6,24 @@
 pack_test_() ->
     [
         {
-            "it should return atom 'ok'",
-            ?_assertEqual(ok, pack([]))
+            "it should wrap in a new list consecutive duplicate elements in the non-empty list",
+            ?_assertEqual(
+                [[a, a, a, a], [b], [c, c], [a, a], [d], [e, e, e, e]],
+                pack([a, a, a, a, b, c, c, a, a, d, e, e, e, e])
+            )
+        },
+        {
+            "it should wrap in a new list elements in the non-empty list",
+            ?_assertEqual(
+                [[a], [b], [c], [a], [d], [e]],
+                pack([a, b, c, a, d, e])
+            )
+        },
+        {
+            "it should return an empty list when you pass an empty list",
+            ?_assertEqual(
+                [],
+                pack([])
+            )
         }
     ].

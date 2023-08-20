f51e56c4a18360019695edc23845b1e21fa3ce18
7006 <22399553+7006@users.noreply.github.com>
2023-08-20 20:04:35 +0000

    Add encode/1 implementation

--- lesson2_task10.erl
+++ lesson2_task10.erl
@@ -1,6 +1,15 @@
 -module(lesson2_task10).
 -export([encode/1]).
 
+-import(lesson2_task05, [reverse/1]).
+
 % Закодувати список за допомогою алгоритму RLE
-encode([]) ->
-    ok.
+encode(List) when is_list(List) ->
+    encode(List, 0, []).
+
+encode([], 0, Acc) ->
+    reverse(Acc);
+encode([H, H | T], Count, Acc) ->
+    encode([H | T], Count + 1, Acc);
+encode([H | T], Count, Acc) ->
+    encode(T, 0, [{Count + 1, H} | Acc]).
---
+++ lesson2_task10.md
@@ -0,0 +1,16 @@
+Example of step by step exection of `encode/1`
+
+|List|Count|Acc|
+|----|-----|---|
+|aaabccccdee|0|[]
+|aabccccdee|1|[]
+|abccccdee|2|[]
+|bccccdee|0|{3,a}
+|ccccdee|0|{1,b} {3,a} 
+|cccdee|1|{1,b} {3,a}
+|ccdee|2|{1,b} {3,a}
+|cdee|3|{1,b} {3,a}
+|dee|0|{4,c} {1,b} {3,a}
+|ee|0|{1,d} {1,b} {3,a}
+|e|1|{1,d} {1,b} {3,a}
+|[]|0|{2,e} {1,d} {1,b} {3,a}

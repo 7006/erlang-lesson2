7b1299abf27d4782754397702a7c999fe7fc0d4c
7006 <22399553+7006@users.noreply.github.com>
2023-08-20 09:56:33 +0000

    Add failed tests for replicate/2

--- lesson2_task15.erl
+++ lesson2_task15.erl
@@ -1,5 +1,9 @@
 -module(lesson2_task15).
--export([replicate/1]).
+-export([replicate/2]).
 
-replicate([]) ->
+% Написати функцію-реплікатор всіх елементів вхідного списку
+replicate(List, N) when
+    is_list(List),
+    is_integer(N) andalso N > 0
+->
     ok.
--- lesson2_task15_tests.erl
+++ lesson2_task15_tests.erl
@@ -1,12 +1,40 @@
 -module(lesson2_task15_tests).
--import(lesson2_task15, [replicate/1]).
+-import(lesson2_task15, [replicate/2]).
 
 -include_lib("eunit/include/eunit.hrl").
 
 replicate_test_() ->
     [
         {
-            "it should return atom 'ok'",
-            ?_assertEqual(ok, replicate([]))
+            "it should replicate consequential elements N times for a non-empty list",
+            [
+                ?_assertEqual(
+                    [a, a, a, a, b, b, b, b, c, c, c, c],
+                    replicate([a, b, c], 4)
+                ),
+                ?_assertEqual(
+                    [a, a, a, b, b, b, c, c, c],
+                    replicate([a, b, c], 3)
+                ),
+                ?_assertEqual(
+                    [a, a, b, b, c, c],
+                    replicate([a, b, c], 2)
+                )
+            ]
+        },
+        {
+            "it should return the same non-empty list when N equals to 1",
+            [
+                ?_assertEqual([a, b, c, d], replicate([a, b, c, d], 1)),
+                ?_assertEqual([a], replicate([a], 1))
+            ]
+        },
+        {
+            "it should return an empty list when you pass an empty list for any value of N",
+            [
+                ?_assertEqual([], replicate([], 1)),
+                ?_assertEqual([], replicate([], 2)),
+                ?_assertEqual([], replicate([], 3))
+            ]
         }
     ].

e20a7c99b9474a5e884b7b120e94e6a1487c5abe
7006 <22399553+7006@users.noreply.github.com>
2023-08-20 09:15:20 +0000

    Add duplicate/1 implementation

--- lesson2_task14.erl
+++ lesson2_task14.erl
@@ -1,6 +1,13 @@
 -module(lesson2_task14).
 -export([duplicate/1]).
 
+-import(lesson2_task05, [reverse/1]).
+
 % Написати дублікатор усіх елементів списку
-duplicate([]) ->
-    ok.
+duplicate(List) when is_list(List) ->
+    duplicate(List, []).
+
+duplicate([], Acc) ->
+    reverse(Acc);
+duplicate([H | T], Acc) ->
+    duplicate(T, [H, H | Acc]).

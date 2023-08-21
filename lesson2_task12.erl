7711420eea91fef9ca3d0fa83f8c39f7f4fe996f
7006 <22399553+7006@users.noreply.github.com>
2023-08-21 09:29:06 +0000

    Add decode_modified/1 implementation

--- lesson2_task12.erl
+++ lesson2_task12.erl
@@ -1,5 +1,17 @@
 -module(lesson2_task12).
 -export([decode_modified/1]).
 
-decode_modified([]) ->
-    ok.
+-import(lesson2_task05, [reverse/1]).
+
+% Написати декодер для модифікованого алгоритму RLE
+decode_modified(List) when is_list(List) ->
+    decode_modified(List, []).
+
+decode_modified([], Acc) ->
+    reverse(Acc);
+decode_modified([{0, _} | T], Acc) ->
+    decode_modified(T, Acc);
+decode_modified([{Count, H} | T], Acc) ->
+    decode_modified([{Count - 1, H} | T], [H | Acc]);
+decode_modified([H | T], Acc) ->
+    decode_modified(T, [H | Acc]).

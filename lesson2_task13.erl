af9c72df2f364e451112ec9c343787e6f64de293
7006 <22399553+7006@users.noreply.github.com>
2023-08-21 11:26:29 +0000

    Add decode/1 implementation

--- lesson2_task13.erl
+++ lesson2_task13.erl
@@ -1,6 +1,15 @@
 -module(lesson2_task13).
 -export([decode/1]).
 
+-import(lesson2_task05, [reverse/1]).
+
 % Написати декодер для стандартного алгоритму RLE
-decode([]) ->
-    ok.
+decode(List) when is_list(List) ->
+    decode(List, []).
+
+decode([], Acc) ->
+    reverse(Acc);
+decode([{0, _} | T], Acc) ->
+    decode(T, Acc);
+decode([{Count, H} | T], Acc) ->
+    decode([{Count - 1, H} | T], [H | Acc]).

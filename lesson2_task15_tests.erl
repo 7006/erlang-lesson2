f36b57bed28509e027258d2717b51811bd35885a
7006 <22399553+7006@users.noreply.github.com>
2023-08-21 15:47:10 +0300

    Adjust tests for replicate/1

--- lesson2_task15_tests.erl
+++ lesson2_task15_tests.erl
@@ -6,7 +6,7 @@
 replicate_test_() ->
     [
         {
-            "it should replicate consequential elements 'Count' times for a non-empty list",
+            "it should replicate each element 'Count' times for a non-empty list",
             [
                 ?_assertEqual(
                     [a, a, a, a, b, b, b, b, c, c, c, c],
@@ -19,22 +19,32 @@ replicate_test_() ->
                 ?_assertEqual(
                     [a, a, b, b, c, c],
                     replicate([a, b, c], 2)
+                ),
+                ?_assertEqual(
+                    [a, b, c, d],
+                    replicate([a, b, c, d], 1)
+                ),
+                ?_assertEqual(
+                    [a],
+                    replicate([a], 1)
                 )
             ]
         },
         {
-            "it should return the same list for a one-element list when Count equals to 1",
+            "it should replicate an empty list as an empty list",
             [
-                ?_assertEqual([a, b, c, d], replicate([a, b, c, d], 1)),
-                ?_assertEqual([a], replicate([a], 1))
-            ]
-        },
-        {
-            "it should return an empty list when you pass an empty list for any value of Count",
-            [
-                ?_assertEqual([], replicate([], 1)),
-                ?_assertEqual([], replicate([], 2)),
-                ?_assertEqual([], replicate([], 3))
+                ?_assertEqual(
+                    [],
+                    replicate([], 1)
+                ),
+                ?_assertEqual(
+                    [],
+                    replicate([], 2)
+                ),
+                ?_assertEqual(
+                    [],
+                    replicate([], 3)
+                )
             ]
         }
     ].

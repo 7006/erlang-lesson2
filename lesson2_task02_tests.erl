5979c240f115c58bd7218a0140a1a124e5d779ac
7006 <22399553+7006@users.noreply.github.com>
2023-08-21 14:55:34 +0300

    Reword tests descriptions

--- lesson2_task02_tests.erl
+++ lesson2_task02_tests.erl
@@ -6,23 +6,23 @@
 but_last_test_() ->
     [
         {
-            "it should return atom 'undefined' for an empty list",
+            "it should return 'undefined' for an empty list",
             ?_assertEqual(undefined, but_last([]))
         },
         {
-            "it should return atom 'undefined' for the one-element list",
+            "it should return 'undefined' for the one-element list",
             ?_assertEqual(undefined, but_last([a]))
         },
         {
-            "it should return 2 last elements for the two-element list",
+            "it should find 2 last elements for the two-element list",
             ?_assertEqual({a, b}, but_last([a, b]))
         },
         {
-            "it should return 2 last elements for the three-element list",
+            "it should find 2 last elements for the three-element list",
             ?_assertEqual({b, c}, but_last([a, b, c]))
         },
         {
-            "it should return 2 last elements for a non empty list",
+            "it should find 2 last elements for a non-empty list",
             ?_assertEqual({e, f}, but_last([a, b, c, d, e, f]))
         }
     ].

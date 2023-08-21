51732cdb7252502edacbdd157b9bcf566ec6b7fe
7006 <22399553+7006@users.noreply.github.com>
2023-08-21 15:30:16 +0300

    Reword tests descriptions

--- lesson2_task09_tests.erl
+++ lesson2_task09_tests.erl
@@ -6,21 +6,21 @@
 pack_test_() ->
     [
         {
-            "it should wrap in a new list consecutive duplicate elements in the non-empty list",
+            "it should pack a non-empty list to a new list in which the groups of duplicates are wrapped in its own lists",
             ?_assertEqual(
                 [[a, a, a, a], [b], [c, c], [a, a], [d], [e, e, e, e]],
                 pack([a, a, a, a, b, c, c, a, a, d, e, e, e, e])
             )
         },
         {
-            "it should wrap in a new list elements in the non-empty list",
+            "it should pack a non-empty no-duplicates list to a new list in which the elements are wrapped in its own list",
             ?_assertEqual(
                 [[a], [b], [c], [a], [d], [e]],
                 pack([a, b, c, a, d, e])
             )
         },
         {
-            "it should return an empty list when you pass an empty list",
+            "it should pack an empty list as an empty list",
             ?_assertEqual(
                 [],
                 pack([])
